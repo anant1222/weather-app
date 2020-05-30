3b3181c9b0c103d2e1091b4c44beea8f62b6f8a0
Anant Yadav <56696253+anant1222@users.noreply.github.com>
2020-05-30 09:22:58 +0530

    initial coment

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "775cbb7dcc44682dbb0f7ed8b9cab68d"
+    }
+}
+
+module.exports = constants;
---
+++ public/js/app.js
@@ -0,0 +1,46 @@
+var fetchWeather = "/weather";
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+const weatherIcon = document.querySelector('.weatherIcon i');
+const weatherCondition = document.querySelector('.weatherCondition');
+
+const tempElement = document.querySelector('.temperature span');
+
+const locationElement = document.querySelector('.place');
+
+const dateElement = document.querySelector('.date');
+
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+dateElement.textContent = new Date().getDate() + ", " + monthNames[new Date().getMonth()].substring(0, 3);
+
+
+weatherForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    locationElement.textContent = "Loading...";
+    tempElement.textContent = "";
+    weatherCondition.textContent = "";
+    const locationApi = fetchWeather + "?address=" + search.value;
+    fetch(locationApi).then(response => {
+        response.json().then(data => {
+            if(data.error) {
+                locationElement.textContent = data.error;
+                tempElement.textContent = "";
+                weatherCondition.textContent = "";
+            } else {
+                console.log()
+                if(data.description === "rain" || data.description === "fog"|| data.description === "haze"||
+                data.description === "mist"||data.description === "scatterd clouds"||data.description === "broken clouds "||data.description === "clear sky"||data.description ==="overcast clouds") {
+                    weatherIcon.className = "wi wi-day-" + data.description
+                } else {
+                    weatherIcon.className = "wi wi-day-cloudy"
+                }
+                locationElement.textContent = data.cityName;
+                tempElement.textContent = (data.temperature - 273.5).toFixed(2) + String.fromCharCode(176);
+                weatherCondition.textContent = data.description.toUpperCase();
+            }
+        }) 
+    });
+})
